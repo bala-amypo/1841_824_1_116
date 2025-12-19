b32e6581cc79221ece190dbd1ce7814740a3faf4
bala-amypo <bala@amypo.in>
2025-12-19 08:49:59 +0000

    User commit

--- src/main/java/com/example/demo/service/UserService.java
+++ src/main/java/com/example/demo/service/UserService.java
@@ -1,29 +1,7 @@
-package com.example.demo.service.serImp;
-import java.util.Set;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
+package com.example.demo.service;
 import com.example.demo.entity.User;
-import com.example.demo.repository.UserRepository;
-import com.example.demo.service.UserService;
 
-@Service
-public class UserSerImp implements UserService{
-    
-    @Autowired
-    UserRepository userRepoObj;
-
-    @Override
-    public void registerUser(String email,String password)
-    {
-        User user=new User(email,password, Set.of("USER"));
-        userRepoObj.save(user);
-    }
-
-    @Override
-    public User getUserByEmail(String email)
-    {
-        return userRepoObj.findByEmail(email);
-    }
+public interface  UserService { 
+    public void registerUser(String email,String password);
+    User getUserByEmail(String email);
 }
--- src/main/java/com/example/demo/service/serImp/UserServiceImp.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.service;
-import com.example.demo.entity.User;
-
-public interface  UserService { 
-    public void registerUser(String email,String password);
-    User getUserByEmail(String email);
-}
