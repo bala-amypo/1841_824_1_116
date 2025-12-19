3558c2a96c03926948f3dd4b78684fc330cd545e
bala-amypo <bala@amypo.in>
2025-12-19 08:31:59 +0000

    User commit

---
+++ src/main/java/entity/User.java
@@ -0,0 +1,70 @@
+package com.example.demo.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import java.time.LocalDateTime;
+import java.util.Set;
+
+import jakarta.persistence.*;
+@Entity
+public class User {
+    @Id 
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private  Long id;
+    @Column(unique=true)
+    private String email;
+
+@JsonIgnore
+private String password;
+
+    @ElementCollection(fetch = FetchType.EAGER)
+@CollectionTable(
+    name = "user_roles",
+    joinColumns = @JoinColumn(name = "user_id")
+)
+@Column(name = "role")
+private Set<String> roles;
+
+    private LocalDateTime createdAt;
+
+    public User()
+    {}
+    public User(
+    String email,
+    String password,
+    Set<String> roles)
+    {
+        this.email=email;
+        this.password=password;
+        this.roles=roles;
+        this.createdAt = LocalDateTime.now();
+    }
+    
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public Set<String> getRoles() {
+        return roles;
+    }
+    public void setRoles(Set<String> roles) {
+        this.roles = roles;
+    }
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+    
+}
+
+
