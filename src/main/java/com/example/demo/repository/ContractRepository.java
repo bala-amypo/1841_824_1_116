747795c66b8bfb05c4084ecbcf65694f7a1e4e38
bala-amypo <bala@amypo.in>
2025-12-23 04:56:05 +0000

    User commit

--- src/main/java/com/example/demo/repository/ContractRepository.java
+++ src/main/java/com/example/demo/repository/ContractRepository.java
@@ -1,9 +1,10 @@
 package com.example.demo.repository;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import com.example.demo.entity.*;
+
+import com.example.demo.entity.Contract;
 
 @Repository
-public interface ContractRepository extends JpaRepository<Contract, Long> 
-{
+public interface ContractRepository extends JpaRepository<Contract, Long> {
 }
--- src/main/java/com/example/demo/service/ContractService.java
+++ src/main/java/com/example/demo/service/ContractService.java
@@ -6,12 +6,8 @@ import com.example.demo.entity.Contract;
 public interface ContractService {
 
     Contract createContract(Contract contract);
-
     Contract updateContract(Long id, Contract contract);
-
     Contract getContractById(Long id);
-
     List<Contract> getAllContracts();
-
     void updateContractStatus(Long id);
 }
