d370237e45ee27f06279fbf1644d8b1132038e87
bala-amypo <bala@amypo.in>
2025-12-26 16:04:22 +0000

    User commit

--- src/main/java/com/example/demo/service/serImp/UserSerImp.java
+++ src/main/java/com/example/demo/service/serImp/UserSerImp.java
@@ -1,29 +1,53 @@
-package com.example.demo.service.serImp;
-import java.util.Set;
+// package com.example.demo.service.serImp;
+// import java.util.Set;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.stereotype.Service;
+
+// import com.example.demo.entity.User;
+// import com.example.demo.repository.UserRepository;
+// import com.example.demo.service.UserService;
+
+// @Service
+// public class UserSerImp implements UserService{
+    
+//     @Autowired
+//     UserRepository userRepoObj;
+
+//     @Override
+//     public void registerUser(String email,String password)
+//     {
+//         User user=new User(email,password, Set.of("USER"));
+//         userRepoObj.save(user);
+//     }
+
+//     @Override
+//     public User getUserByEmail(String email)
+//     {
+//         return userRepoObj.findByEmail(email);
+//     }
+// }
+package com.example.demo.service.serImp;
 
 import com.example.demo.entity.User;
+import com.example.demo.exception.ResourceNotFoundException;
 import com.example.demo.repository.UserRepository;
 import com.example.demo.service.UserService;
 
-@Service
-public class UserSerImp implements UserService{
-    
-    @Autowired
-    UserRepository userRepoObj;
+public class UserSerImp implements UserService {
 
-    @Override
-    public void registerUser(String email,String password)
-    {
-        User user=new User(email,password, Set.of("USER"));
-        userRepoObj.save(user);
+    private final UserRepository userRepository;
+
+    public UserSerImp(UserRepository userRepository) {
+        this.userRepository = userRepository;
     }
 
     @Override
-    public User getUserByEmail(String email)
-    {
-        return userRepoObj.findByEmail(email);
+    public User getUserByEmail(String email) {
+
+        return userRepository.findByEmail(email)
+                .orElseThrow(() ->
+                        new ResourceNotFoundException("User not found with email: " + email));
     }
 }
+
