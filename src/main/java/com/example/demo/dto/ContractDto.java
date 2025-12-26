3ea46a826a2e48521482a7009da70c5a382ac7a5
bala-amypo <bala@amypo.in>
2025-12-26 15:49:23 +0000

    User commit

---
+++ src/main/java/com/example/demo/dto/ContractDto.java
@@ -0,0 +1,10 @@
+package com.example.demo.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class ContractDto {
+    private String contractNumber;
+    private String title;
+}
